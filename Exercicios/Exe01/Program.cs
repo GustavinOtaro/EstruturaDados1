84d89815afa629f5b4195a9bbf1eb8af2973c618
GustavinOtaro <gustavotite99@gmail.com>
2023-08-30 20:33:24 -0300

    Primeiro Commit

---
+++ Exercicios/Exe01/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] vetor= {0,1,2,3,4,5,6,7,8,9}
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine($"vetor[{i}] = {vetor[i]}");
+}
+
+Console.WriteLine("Ordenado");
+
+for(int i = 0; i < vetor.Length - 1; i++)
+{
+    for(int j = i + 1; j < vetor.Length; j++)
+    {
+        int aux = vetor[j];
+        if(vetor[j]< vetor[i])
+        {
+            vetor[j] = vetor[i];
+            vetor[i] = aux;
+        }
+    }
+}
+
+
+
+
+
+
+
