5f311a233f725d980f5994b9eae4ab422f23ed99
GustavinOtaro <gustavotite99@gmail.com>
2023-10-04 17:17:59 -0300

    Tabela

---
+++ Exercicios/TabelaHash/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, string> tabela = new Dictionary<string, string>(100);
+
+        while (true)
+        {
+            Console.WriteLine("Insira o CPF:");
+            string cpf = Console.ReadLine();
+
+            if (tabela.ContainsKey(cpf))
+            {
+                Console.WriteLine("Chave já inserida.");
+                continue;
+            }
+
+            Console.WriteLine("Insira o nome:");
+            string nome = Console.ReadLine();
+
+            tabela.Add(cpf, nome);
+
+            Console.WriteLine("Deseja inserir mais um par de chave/valor? (S/N)");
+            string resposta = Console.ReadLine();
+
+            if (resposta.ToUpper() != "S")
+            {
+                break;
+            }
+        }
+
+        foreach (var item in tabela)
+        {
+            Console.WriteLine("CPF: {0}, Nome: {1}", item.Key, item.Value);
+        }
+    }
+}
